9547291aba418ffdcdd32acb47f8b98b5afd4fc7
Szymon Warszawa <szymonwarszawa@gmail.com>
2020-11-19 19:36:16 +0000

    Complete all variables for the story.

--- MadLibs.java
+++ MadLibs.java
@@ -3,6 +3,20 @@ public class MadLibs {
      * This program generates a mad libbed story.
      */
     public static void main(String[] args) {
+        String name1 = "Charlie";
+        String name2 = "Zuzana";
+        String adjective1 = "big";
+        String adjective2 = "weird";
+        String adjective3 = "tiny";
+        String noun1 = "mashroom";
+        String noun2 = "rock";
+        String noun3 = "fence";
+        String noun4 = "rainbow";
+        String noun5 = "monkey";
+        String noun6 = "nose";
+        String place1 = "Barcelona";
+        String verb1 = "eat";
+        int number = 7;
 
         // The template for the story
         String story = "This morning " + name1 + " woke up feeling " + adjective1 + ". 'It is going to be a "
@@ -11,5 +25,7 @@ public class MadLibs {
                 + noun4 + "s very " + adjective3 + ". Concerned, " + name1 + " texted " + name2 + ", who flew " + name1
                 + " to " + place1 + " and dropped " + name1 + " in a puddle of frozen " + noun5 + ". " + name1
                 + " woke up in the year " + number + ", in a world where " + noun6 + "s ruled the world.";
+
+        System.out.println(story);
     }
 }
